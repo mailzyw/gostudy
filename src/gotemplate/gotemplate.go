75a75ff05da227f538a9ea3aba64b86e9cd731f6
zhangyiwen <zhangyiwen@youzan.com>
2020-04-10 20:25:45 +0800

    add comment

--- src/gotemplate/gotemplate.go
+++ src/gotemplate/gotemplate.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 )
 
 const c = "C"
@@ -15,9 +17,16 @@ func init() { // initialization of package
 
 func main() {
 	var a int
+	b := 100
 	Func1()
 	// ...
 	fmt.Println(a)
+
+	var goos string = runtime.GOOS
+	fmt.Printf("The operating system is %s\n", goos)
+	path := os.Getenv("PATH")
+	fmt.Printf("Path is %s\n", path)
+	fmt.Println("Hello:", 23, 24, "aaa", b)
 }
 
 func (t T) Method1() {
